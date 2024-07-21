5af1a218a5dc73ae093cae61ca00422b00b03abc
purnimakumari28 <93967948+purnimakumari28@users.noreply.github.com>
2024-07-22 04:50:23 +0530

    add dark mode

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     hamburgerMenu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+function toggleDarkMode() {
+    const body = document.body;
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    
+    body.classList.toggle('dark-mode');
+    
+    if (body.classList.contains('dark-mode')) {
+        toggleButton.textContent = '🌑'; // Black moon emoji for dark mode
+        toggleButton.style.color = 'black';
+    } else {
+        toggleButton.textContent = '🌕'; // Yellow moon emoji for light mode
+        toggleButton.style.color = 'yellow';
+    }
+}
